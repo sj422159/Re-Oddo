4b40fd420a4e271867e9fd677d6db1f3f6da2ebe
Ghosty00ace <wanderingghost679@gmail.com>
2025-07-12 13:32:27 +0530

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -69,5 +69,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect('/');
+        
     }
 }
